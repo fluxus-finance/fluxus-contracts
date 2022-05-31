20c6448f8eb2128b54cce092d3b6deef7a118562
pmarangone <patrickmarangone@gmail.com>
2022-05-31 20:35:15 -0300

    chore: added `args_encoded` to croncat call

--- scripts/auto_compound.sh
+++ scripts/auto_compound.sh
@@ -7,12 +7,14 @@ source .env
 near call $CONTRACT_NAME add_allowed_account '{"account_id" : "'$croncat_manager'"}' --accountId $CONTRACT_NAME
 near call $CONTRACT_NAME get_allowed_accounts '{}' --accountId $CONTRACT_NAME
 
+export args='{"token_id": "'$token_id'"}'
+export args_encoded=$(echo $args | base64)
 
 #### Start croncat tasks responsible for auto-compound 
-# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "claim_reward","cadence": "* 0 * * * *","recurring": true,"deposit": "0","gas": 240000000000000}' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000 
-# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "withdraw_of_reward","cadence": "* 3 * * * *","recurring": true,"deposit": "1","gas": 240000000000000}' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000 
-# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "autocompounds_swap","cadence": "* 6 * * * *","recurring": true,"deposit": "1","gas": 240000000000000 }' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000
-# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "autocompounds_liquidity_and_stake","cadence": "* 9 * * * *","recurring": true,"deposit": "0","gas": 240000000000000}' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000
+# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "claim_reward","cadence": "* 30 * * * *","recurring": false,"deposit": "0","gas": 240000000000000, "arguments":"'$args_encoded'"}' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000 
+# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "withdraw_of_reward","cadence": "* 33 * * * *","recurring": false,"deposit": "0","gas": 240000000000000, "arguments":"'$args_encoded'"}' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000 
+# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "autocompounds_swap","cadence": "* 36 * * * *","recurring": false,"deposit": "0","gas": 240000000000000, "arguments":"'$args_encoded'"}' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000
+# near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "autocompounds_liquidity_and_stake","cadence": "* 39 * * * *","recurring": false,"deposit": "0","gas": 240000000000000, "arguments":"'$args_encoded'"}' --accountId $CONTRACT_NAME --amount 1 --gas 300000000000000
 
 #### Functions managed by auto-compound
 near call $CONTRACT_NAME claim_reward '{"token_id": "'$token_id'"}' --accountId $CONTRACT_NAME --gas $total_gas
