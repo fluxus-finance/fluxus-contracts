93e28041787af8802c38a883ce5f4b727314702a
pmarangone <patrickmarangone@gmail.com>
2022-06-07 15:03:29 -0300

    chore: improve `views.sh` readability

--- scripts/views.sh
+++ scripts/views.sh
@@ -7,29 +7,28 @@ echo $CONTRACT_NAME
 source .env
 
 #### consult if farm is running
-# near view $farm_contract_id get_farm '{ "farm_id": "'$exchange_contract_id'@'$pool_id'#'$farm_id'" }'
+near view $farm_contract_id get_farm '{ "farm_id": "'$exchange_contract_id'@'$pool_id'#'$farm_id'" }'
 
-# #### consult min deposit for seed
-# near view $farm_contract_id get_seed_info '{ "seed_id": "'$exchange_contract_id'@'$pool_id'" }'
+#### consult min deposit for seed
+near view $farm_contract_id get_seed_info '{ "seed_id": "'$exchange_contract_id'@'$pool_id'" }'
 
 #### Get the current contract state
-# near view $CONTRACT_NAME get_contract_state
+near view $CONTRACT_NAME get_contract_state
 
 #### Get exchange and farm contracts
-# near view $CONTRACT_NAME get_contract_info
+near view $CONTRACT_NAME get_contract_info
 
-# near view $CONTRACT_NAME get_allowed_tokens '{}'
+#### Get tokens_id, :10, managed by the contract
+near view $CONTRACT_NAME get_allowed_tokens '{}'
 
-# near view $CONTRACT_NAME get_strats '{}'
+#### Get all strats infos, such as state, token_id, reward_token, min_deposit
+near view $CONTRACT_NAME get_strats '{}'
 
-# near view $CONTRACT_NAME get_strat_state '{"token_id": "'$token_id'" }'
+#### Get state from strat, if its Running, Ended, etc.
+near view $CONTRACT_NAME get_strat_state '{"token_id": "'$token_id'" }'
 
-# near call $CONTRACT_NAME get_strats_info '{}' --accountId $CONTRACT_NAME
+#### Only used by contracts admins, returns the same as get_stras plus current users infos
+near call $CONTRACT_NAME get_strats_info '{}' --accountId $CONTRACT_NAME
 
-# near view $CONTRACT_NAME get_user_shares '{ "account_id": "'$username'", "token_id": ":'$pool_id'" }'
-
-# near view $exchange_contract_id get_pool_shares '{ "pool_id": 410, "account_id": "mury.testnet" }'
-
-# near view $exchange_contract_id get_pool '{ "pool_id": 466 }'
-
-near view $farm_contract_id list_farms_by_seed '{ "seed_id": "'$seed_id'" }'
+#### Returns struct from user {deposited: x, total: y}
+near view $CONTRACT_NAME get_user_shares '{ "account_id": "'$username'", "token_id": ":'$pool_id'" }'
